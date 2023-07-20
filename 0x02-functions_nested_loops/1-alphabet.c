f0fbdb631546b0d87adb50713c6f038e97b9a9b6
lexzy77 <lexzy777@gmail.com>
2023-07-19 17:09:27 -0700

    conbine code

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_alphabet - the alphabet, in lowercase, followed by a new line.
+* Return: Always 0 (Success)
+*
+*/
+
+
+void print_alphabet(void)
+
+{
+int n;
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
